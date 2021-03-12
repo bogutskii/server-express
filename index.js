0cb9ce8bc4ffc5854559906c479916258204e102
bogutskii <sl9site@gmail.com>
2021-03-11 23:33:16 -0800

    -eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
-const PORT = 5000
+const express = require('express');
+const app = express();
+const PORT = 5000;
 
-const home =  (req, res) => {
-    res.send('Hello World!')
-}
+const home = (req, res) => {
+  res.send('Hello World!');
+};
 
-app.get('/', home)
+app.get('/', home);
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});  
