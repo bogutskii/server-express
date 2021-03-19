f4da2c956ff32077cdad273f5664ebdf205da7c2
bogutskii <sl9site@gmail.com>
2021-03-18 20:04:02 -0700

    -mongoose

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import logger from './modules/core/logger';
 import parseResponse from './modules/core/parseResponse';
 import cors from './modules/core/cors';
 import routes from './modules/core/routes';
+import dbConnect from './modules/core/db';
 
 const app = express();
 const PORT = 5000;
-
+dbConnect();
 logger(app);
 parseResponse(app);
 cors(app);
---
+++ src/modules/core/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+function dbConnect() {
+  mongoose.connect('mongodb://localhost/express', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('CONNECTED');
+  });
+}
+
+export default dbConnect;
